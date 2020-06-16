e06103e0897c9e8d49236bbed0bfeef3009028ea
Andrés Rubiano <andresfrm2210@gmail.com>
2020-06-16 16:26:12 -0500

    Uploads configurations

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 class Config():
     DEBUG = False
     SECRET_KEY = "PROBE"
+    UPLOAD_FOLDER = r".\_uploads"
+    MAX_CONTENT_LENGHT = 16 * 1024 * 1024
 
 class DevelopmentConfig(Config):
     DEBUG = True
