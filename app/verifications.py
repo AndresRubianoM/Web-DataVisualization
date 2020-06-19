e1d36578b6236780fd3a5959e76dfdebe2af785c
Andrés Rubiano <andresfrm2210@gmail.com>
2020-06-18 22:24:08 -0500

    correct of the extensions

--- app/verifications.py
+++ app/verifications.py
@@ -1,5 +1,5 @@
 #Possible extensions
-ALLOWED_EXTENSIONS = ['csv', 'json', 'xml', 'html']
+ALLOWED_EXTENSIONS = ['csv', 'json', 'xlsx', 'html']
 
 def verify_extensions(file_name):
     return file_name.split('.')[-1].lower() in ALLOWED_EXTENSIONS
