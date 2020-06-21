3d851e3403f122e2d396509e299dd6b51e145cb2
Andrés Rubiano <andresfrm2210@gmail.com>
2020-06-21 00:25:12 -0500

    complete class to trade the data files

--- app/transform_to_list.py
+++ app/transform_to_list.py
@@ -1,12 +1,13 @@
 import os 
 import json
 import openpyxl
+from bs4 import BeautifulSoup
 
 
 class Transform:
     def __init__(self, filename, route = None):
         self.file = filename
-        self.route = route
+        self.route = '.' + route
         self.extension = self.file.split('.')[-1]
         self._path = self._absolute_path()
         self.list_values = self._proceed()
@@ -16,8 +17,11 @@ class Transform:
         path = self._absolute_path()
         with open(self._path, 'r') as file:
             data = file.readlines()
+
             for line in data:
+                line = line.replace('\n', '')
                 rows_list.append(line.split(','))
+
         return rows_list
     
     def list_from_xlsx(self):
@@ -26,10 +30,11 @@ class Transform:
         rows_list = []
 
         for row in sheet.iter_rows(max_row = sheet.max_row):
-            cells_rows = []
+            value_list = []
+            
             for cell in row:
-                cells_rows.append(cell.value)
-            rows_list.append(cells_rows)
+                value_list.append(cell.value)
+            rows_list.append(value_list)
         
         return rows_list
     
@@ -43,6 +48,27 @@ class Transform:
             
         return rows_list
 
+    def list_from_html(self):
+        rows_list = []
+        with open(self._path, 'r') as file:
+            data = file.read()
+            
+            soup = BeautifulSoup(data, 'lxml')
+            rows = soup.find('table').find_all('tr')
+
+            for row in rows:
+                value_list = []
+                lines = row.find_all(['td', 'th'])
+
+                for line in lines:
+                    value_list.append(line.getText())
+
+                rows_list.append(value_list)
+
+            return rows_list
+
+
+
     def _proceed(self):
         '''Select the procedure to each of the valid extensions'''
         if self.extension == 'csv':
@@ -51,12 +77,13 @@ class Transform:
             return  self.list_from_xlsx()
         elif self.extension == 'json':
             return  self.list_from_json()
+        elif self.extension == 'html':
+            return self.list_from_html()
         else:
             return None
 
         
-
-                       
+                
     def _absolute_path(self):
         '''Define the absolute path to de file'''
         if self.route is not None:
@@ -73,62 +100,5 @@ class Transform:
     
 
 
-if __name__ == "__main__":
-    route = r'..\_uploads'
-    file =  'mtcarsjson.json'
-
-    my_path = os.path.abspath(os.path.dirname(__file__))
-    path_relative_file = os.path.join(route ,  file)
-    #Absolute path to the data file
-    path = os.path.join(my_path, path_relative_file)
-
-    words = Transform(file, route)
-    print(words.list_from_json())
-    '''#
-    rows_list = []
-    with open(path, 'r') as file:
-        data = json.load(file)
-        print(list(data[1].keys()))
-        rows_list.append(list(data[1].keys()))
-        for line in data:
-            rows_list.append(list(line.values()))
-            
-        print(rows_list)'''
-
-
-
-
-
-
-    """ rows_list = []
-    
-
-
-    file = openpyxl.load_workbook(path)
-    sheet = file.active
-    #print(sheet)
-    #print(sheet.max_row, sheet.max_column)
-    for row in sheet.iter_rows(max_row = sheet.max_row):
-        cells_sheet = []
-        for cell in row:
-            cells_sheet.append(cell.value)
-        rows_list.append(cells_sheet)
-        #print(rows_list) """
-        
-
-
-    """ import os 
-    #Asolute path to the current script
-    my_path = os.path.abspath(os.path.dirname(__file__))
-    #Absolute path to the data file
-    path = os.path.join(my_path, r'..\_uploads\mtcars.csv')
-    
-    var = []
-
-    with open(path, 'r') as file:
-        data = file.readlines()
-        for line in data:
-            var.append(line.split(',')) """
-            
 
         
