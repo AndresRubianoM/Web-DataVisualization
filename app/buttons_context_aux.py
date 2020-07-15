c5bb2635b79483554c4e104f9c9745c0a154c17b
Andrés Rubiano <andresfrm2210@gmail.com>
2020-07-14 19:20:09 -0500

    function of the buttons variables finished

--- app/buttons_context_aux.py
+++ app/buttons_context_aux.py
@@ -1,22 +1,36 @@
 import copy
 
-def context_buttons(columns_per_action, default):
+def context_buttons(columns_add, columns_remove, default):
     '''Define the columns possible for the add button, the default values are all the columns
     columns_per_action: List (columns passed by the add button)
     default: List (columns of the table)'''
 
-    if columns_per_action is not None:
+    if columns_add is not None:
+        #Define the values in the add button with the columns_add
         data_add = copy.deepcopy(default)
-        for col in columns_per_action:
+        for col in columns_add:
             if col in default:
                 data_add.remove(col)
 
-        data_remove = columns_per_action
+        #Looking the possible columns to remove, redefine the values for each button, Actualize columns of columns_add and columns_remove
+        if columns_remove is not None:
+            data_remove = columns_add
+
+            for col in columns_remove:
+                if col not in data_add:
+                    data_add.append(col)
+                    data_remove.remove(col) 
+                    columns_remove.remove(col)
+        else:
+            #if dont exist columns_remove then the possible values to erase are the values added 
+            data_remove = columns_add
+
     else:
+        #By default all the columns are possible values to add
         data_add = default
         data_remove = []
 
-    return data_add, data_remove
+    return data_add, data_remove, columns_add, columns_remove
 
 
 
